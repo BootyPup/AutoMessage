575ac537f66474d64342b6b767425d2f3e4b0fec
fluffybunny11 <creepyhobo101@gmail.com>
2016-07-17 17:39:56 -0400

    Update MessageAnnouncerConfiguration.cs

--- MessageAnnouncer-master/MessageAnnouncerConfiguration.cs
+++ MessageAnnouncer-master/MessageAnnouncerConfiguration.cs
@@ -47,10 +47,11 @@ namespace fr34kyn01535.MessageAnnouncer
 
         public void LoadDefaults()
         {
-            Interval = 180;
+            Interval = 600000;
             Messages = new Message[]{
                 new Message("Welcome to Fluffy's Zombie House, we hope you enjoy your stay!"),
-                new Message("Join our TeamSpeak 3 server at 66.55.149.29:9197"),
+                new Message("Join our TeamSpeak 3 server and meet some new friends at 66.55.149.29:9197"),
+                new Message("Curious about our rules? View then on our website http://fluffyszombiehouse.weebly.com/"),
                 new Message("If you have any questions ask an admin on our TeamSpeak 3 server!"),
                 new Message("Please chat in english. Be polite."),
             };
@@ -58,7 +59,9 @@ namespace fr34kyn01535.MessageAnnouncer
                 new TextCommand(){Name="rules",Help="Shows the server rules",Text = new List<string>(){
                     "#1 No bug using, exploiting or abuse of powers",
                     "#2 Don't ask admins for items, loot respawn, ect.",
-                    "#3 Please speak english in the public chat"}
+                    "#3 Please speak english in the public chat",
+                    "#4 Join our TeamSpeak 3 server and meet some new friends at 66.55.149.29:9197"
+                }
                 }
             };
         }
